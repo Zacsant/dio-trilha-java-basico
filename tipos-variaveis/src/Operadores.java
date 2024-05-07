4e7650f51e49961a32d9b41113eb1feb2dcef93b
Zacsant <santiago6590@gmail.com>
2024-05-07 17:04:01 -0300

    exercícios aula operadoress 6

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,18 @@
+public class Operadores {
+    
+    public static void main(String[] args) {
+
+        int numero1 = 2;
+        int numero2 = 4;
+
+        Boolean sinNao = numero1 == numero2;
+
+        System.out.println("numeroUm é igual a numeroDois? " + sinNao);
+
+        sinNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente a numeroDois? " + sinNao);
+
+
+    }
+}
